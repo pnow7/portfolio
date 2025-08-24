310b48bf4e60d0f0a496c6fba01abdc3de5b797b
guswo4324 <phj2394324@naver.com>
2025-08-24 14:25:49 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ProjectsPage from './pages/ProjectsPage';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/projects" element={<ProjectsPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/data/projects.js
@@ -0,0 +1,9 @@
+export const myProjects = [
+  {
+    id: 1,
+    title: '동해안 맛집 웹 서비스',
+    description: '동해안 맛집 정보 공유 플랫폼”**은 고성부터 부산까지 7번 국도 중심의 지역 맛집, 특산물, 문화 정보를 제공하고, 여행객과 현지인이 함께 소통하며 추천할 수 있는 웹 기반 서비스를 개발하였습니다.',
+    image: '/images/restaurant_project.jpg',
+    skills: ['IntelliJ IDEA', 'VS Code', 'HTML', 'Axios', 'Python', 'SpringBoot', 'Oracle', 'Java', 'MyBatis', 'RestAPI', 'sql', 'React', 'CSS', 'JavaScript', 'Kakao Maps API', 'OpenAi', 'GitHub'],
+  }
+];
