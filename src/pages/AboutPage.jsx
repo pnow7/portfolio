7e41073c86226dee13d7356f623ea94a7375bd9c
현재 <phyunjae7333@gmail.com>
2025-09-29 21:00:53 +0900

    [feat] 별 parallax 추가 및 About Me 수정 중
    Signed-off-by: 현재 <phyunjae7333@gmail.com>

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
@@ -8,18 +7,27 @@ const hashtags = [
 ];
 
 const frontendSkills = [
-	"React", "HTML", "CSS", "JSP", "Figma"
+	{ name: "React", icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" },
+	{ name: "HTML", icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg" },
+	{ name: "CSS", icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg" },
+	{ name: "JSP", icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg" },
+	{ name: "Figma", icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/figma/figma-original.svg" },
 ];
+
 const backendSkills = [
-	"Java", "Spring", "Spring Boot", "Oracle", "Python"
+	{ name: "Java", icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg" },
+	{ name: "Python", icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg" },
+	{ name: "JavaScript", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg" },
+	{ name: "Spring", icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/spring/spring-original.svg" },
+	{ name: "Oracle", icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/oracle/oracle-original.svg" },
+	{ name: "SQL Developer", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/sqldeveloper/sqldeveloper-original.svg" },
 ];
 
 const AboutPage = ({ onlySection }) => (
 	onlySection ? (
 		<div className="main-section">
-			<section className="about-section">
+			<section className="about-section" style={{ textAlign: "center" }}>
 				<div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-					{/* 3D 스타일 캐릭터(오픈소스 SVG) */}
 					<img
 						src="https://avatars.githubusercontent.com/u/9919?s=200&v=4"
 						alt="3D Character"
@@ -31,7 +39,7 @@ const AboutPage = ({ onlySection }) => (
 						사용자 경험과 효율적인 시스템 설계를 중요하게 생각하며,<br />
 						다양한 기술을 활용해 문제를 해결하는 것을 즐깁니다.
 					</p>
-					<div className="hashtag-list" style={{ marginBottom: 18 }}>
+					<div className="hashtag-list" style={{ marginBottom: 18, justifyContent: "center" }}>
 						{hashtags.map(tag => (
 							<span key={tag} className="hashtag">{tag}</span>
 						))}
@@ -47,14 +55,16 @@ const AboutPage = ({ onlySection }) => (
 						GitHub
 					</a>
 				</div>
-				<div className="skills-tools-area">
+				<div className="skills-tools-area" style={{ textAlign: "center" }}>
 					<h3>Skill &amp; Tools</h3>
 					<div className="skills-tools-grid">
 						<div className="skills-col">
 							<h4>Frontend</h4>
 							<ul className="skills-list">
 								{frontendSkills.map(skill => (
-									<li key={skill}>{skill}</li>
+									<li key={skill.name}>
+										<img src={skill.icon} alt={skill.name} style={{ width: 40, height: 40 }} />
+									</li>
 								))}
 							</ul>
 						</div>
@@ -62,7 +72,9 @@ const AboutPage = ({ onlySection }) => (
 							<h4>Backend</h4>
 							<ul className="skills-list">
 								{backendSkills.map(skill => (
-									<li key={skill}>{skill}</li>
+									<li key={skill.name}>
+										<img src={skill.icon} alt={skill.name} style={{ width: 40, height: 40 }} />
+									</li>
 								))}
 							</ul>
 						</div>
@@ -74,9 +86,8 @@ const AboutPage = ({ onlySection }) => (
 		<>
 			<Header />
 			<main className="main-section">
-				<section className="about-section">
+				<section className="about-section" style={{ textAlign: "center" }}>
 					<div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-						{/* 3D 스타일 캐릭터(오픈소스 SVG) */}
 						<img
 							src="https://avatars.githubusercontent.com/u/9919?s=200&v=4"
 							alt="3D Character"
@@ -88,7 +99,7 @@ const AboutPage = ({ onlySection }) => (
 							사용자 경험과 효율적인 시스템 설계를 중요하게 생각하며,<br />
 							다양한 기술을 활용해 문제를 해결하는 것을 즐깁니다.
 						</p>
-						<div className="hashtag-list" style={{ marginBottom: 18 }}>
+						<div className="hashtag-list" style={{ marginBottom: 18, justifyContent: "center" }}>
 							{hashtags.map(tag => (
 								<span key={tag} className="hashtag">{tag}</span>
 							))}
@@ -101,17 +112,19 @@ const AboutPage = ({ onlySection }) => (
 							style={{ marginBottom: 24 }}
 						>
 							<svg width="20" height="20" fill="currentColor" style={{ verticalAlign: "middle", marginRight: 6 }} viewBox="0 0 24 24"><path d="M12 .5C5.73.5.5 5.73.5 12c0 5.08 3.29 9.39 7.86 10.91.58.11.79-.25.79-.56 0-.28-.01-1.02-.02-2-3.2.7-3.88-1.54-3.88-1.54-.53-1.34-1.3-1.7-1.3-1.7-1.06-.72.08-.71.08-.71 1.17.08 1.78 1.2 1.78 1.2 1.04 1.78 2.73 1.27 3.4.97.11-.75.41-1.27.74-1.56-2.55-.29-5.23-1.28-5.23-5.7 0-1.26.45-2.29 1.19-3.1-.12-.29-.52-1.46.11-3.05 0 0 .97-.31 3.18 1.18a11.1 11.1 0 0 1 2.9-.39c.98 0 1.97.13 2.9.39 2.2-1.49 3.17-1.18 3.17-1.18.63 1.59.23 2.76.11 3.05.74.81 1.19 1.84 1.19 3.1 0 4.43-2.69 5.41-5.25 5.7.42.36.79 1.09.79 2.2 0 1.59-.01 2.87-.01 3.26 0 .31.21.68.8.56C20.71 21.39 24 17.08 24 12c0-6.27-5.23-11.5-12-11.5z"/></svg>
-							GitHub
-						</a>
+						GitHub
+					</a>
 					</div>
-					<div className="skills-tools-area">
+					<div className="skills-tools-area" style={{ textAlign: "center" }}>
 						<h3>Skill &amp; Tools</h3>
 						<div className="skills-tools-grid">
 							<div className="skills-col">
 								<h4>Frontend</h4>
 								<ul className="skills-list">
 									{frontendSkills.map(skill => (
-										<li key={skill}>{skill}</li>
+										<li key={skill.name}>
+											<img src={skill.icon} alt={skill.name} style={{ width: 40, height: 40 }} />
+										</li>
 									))}
 								</ul>
 							</div>
@@ -119,7 +132,9 @@ const AboutPage = ({ onlySection }) => (
 								<h4>Backend</h4>
 								<ul className="skills-list">
 									{backendSkills.map(skill => (
-										<li key={skill}>{skill}</li>
+										<li key={skill.name}>
+											<img src={skill.icon} alt={skill.name} style={{ width: 40, height: 40 }} />
+										</li>
 									))}
 								</ul>
 							</div>
