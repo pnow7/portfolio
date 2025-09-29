dba14107dd595bed97772bfd65ea548b6c3e9f06
현재 <phyunjae7333@gmail.com>
2025-09-29 19:28:44 +0900

    메인페이지 작업 중
    Signed-off-by: 현재 <phyunjae7333@gmail.com>

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,8 @@
+// 별 배경 parallax 효과
+window.addEventListener('scroll', () => {
+  const y = window.scrollY;
+  document.body.style.setProperty('--star-bg-pos', `0px ${y * 0.2}px`);
+});
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
