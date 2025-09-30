bef439862913fafd543659efaea8e89bdf6027cb
현재 <phyunjae7333@gmail.com>
2025-10-01 02:26:31 +0900

    feat: header 카테고리 목록 -> 영어로 변경

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,11 +7,11 @@ const Header = () => (
 				<a href="#hero" className="logo-text">Pnow7 Portfolio</a>
 			</div>
 			<ul className="nav-links">
-				<li><a href="#hero">홈</a></li>
-				<li><a href="#about">소개</a></li>
-				<li><a href="#projects">프로젝트</a></li>
-				<li><a href="#education">학력 및 교육</a></li> 
-				<li><a href="#contact">연락처</a></li>
+				<li><a href="#hero">Home</a></li>
+				<li><a href="#about">About Me</a></li>
+				<li><a href="#projects">Projects</a></li>
+				<li><a href="#education">Education</a></li> 
+				<li><a href="#contact">Contact</a></li>
 			</ul>
 		</nav>
 	</header>
